d5678d746d9fe27583867c5ac52f60b6cce6c497
Dhruv-vs <127077342+Dhruv-vs@users.noreply.github.com>
2025-03-13 16:46:11 +0530

    Update Reverse a linked list.cpp

--- GFG Questions/Linked List/Reverse a linked list.cpp
+++ GFG Questions/Linked List/Reverse a linked list.cpp
@@ -20,3 +20,24 @@ class Solution {
         reverse(head);
     }
 };
+
+// second method of reverse using loops
+void print(Node *prev){
+    while(prev!=NULL){
+        cout<<prev->data<<" ";
+        prev=prev->next;
+    }
+}
+    Node* reverseList(struct Node* head) {
+        // reverse(head);
+        Node *prev= NULL;
+        Node *temp;
+        while(head!=NULL){
+            temp = head->next;
+            head->next= prev;
+            prev=head;
+            head=temp;
+        }
+        print(prev);
+    }
+};

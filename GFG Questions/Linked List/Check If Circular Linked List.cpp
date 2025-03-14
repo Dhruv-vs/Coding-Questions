45969f9131d2cc5e0a432dd9c67b1e8a76bc915a
Dhruv-vs <127077342+Dhruv-vs@users.noreply.github.com>
2025-03-14 18:13:18 +0530

    Update Check If Circular Linked List.cpp

--- GFG Questions/Linked List/Check If Circular Linked List.cpp
+++ GFG Questions/Linked List/Check If Circular Linked List.cpp
@@ -13,3 +13,16 @@ class Solution {
         return false;
     }
 };
+
+// second method
+class Solution {
+  public:
+    bool isCircular(Node *head) {
+         if (head == NULL) return false;
+        Node *temp = head;
+        while (temp->next != NULL && temp->next != head) {
+            temp = temp->next;
+        }
+        return (temp->next == head);
+    }
+};

2d8f92a4e45db41859603c5d900cdd90abf27e0f
Dhruv-vs <127077342+Dhruv-vs@users.noreply.github.com>
2025-05-19 14:47:40 +0530

    Create Mirror tree.cpp

---
+++ GFG Questions/Trees/Mirror tree.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    
+    void mirror(Node* root) {
+    if(!root) return;
+    
+    mirror(root->left);
+    mirror(root->right);
+    
+    swap(root->left,root->right);
+    
+    }
+};

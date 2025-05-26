e3aea8947cee229ce48fc389277f8c7168486004
Dhruv-vs <127077342+Dhruv-vs@users.noreply.github.com>
2025-05-26 22:37:57 +0530

    Create Max Path Sum 2 Special Nodes.cpp

---
+++ GFG Questions/TreesHard/Max Path Sum 2 Special Nodes.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+  int find(Node* root,int &sum){
+      if(!root) return 0;
+      if(!root->left && !root->right) return root->data;
+      
+      int left = find(root->left,sum);
+      int right = find(root->right,sum);
+
+    // if both left and right exist then only update sum
+      if(root->left && root->right){
+      sum = max(sum,left+right+root->data);
+      return max(root->data+left, root->data+right);
+      }
+    // if not then return what exist with current node
+      if(root->left){
+          return root->data+left;
+      }
+      else return root->data+right;
+  }
+    int maxPathSum(Node* root) {
+      int sum = INT_MIN;
+      int val = find(root,sum);
+      // if the sum is not updated means no left right in entire tree then return the single path sum
+      if (sum == INT_MIN)
+            return val;
+      return sum;
+    }
+}; 

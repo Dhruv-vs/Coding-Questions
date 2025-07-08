aed2186fd5547c60522fc7deb921fdf912bf02c6
Dhruv-vs <127077342+Dhruv-vs@users.noreply.github.com>
2025-07-08 19:42:14 +0530

    Create Merge K sorted array.cpp

---
+++ Interview Bit/Merge K sorted array.cpp
@@ -0,0 +1,30 @@
+vector<int> Solution::solve(vector<vector<int> > &A) {
+    vector<int>ans;
+    int row = A.size();
+    int col = A[0].size();
+    
+    // creating min heap
+    // priority_queue<int,vector<int>,greater<int> >minheap; // simple minheap
+    
+    // mean contains pair 
+    priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>> >minheap;
+    
+    for(int i=0;i<row;i++) minheap.push(make_pair(A[i][0],make_pair(i,0)));
+    
+    pair<int,pair<int,int>>p;
+    while(!minheap.empty())
+    {
+    p = minheap.top(); // copying top in pair p
+    // get min 
+    ans.push_back(p.first);
+    row = p.second.first;
+    col = p.second.second;
+    
+    minheap.pop();
+    
+    // insert if col is valid in minheap
+    if(col<A[0].size()-1)
+    minheap.push(make_pair(A[row][col+1],make_pair(row,col+1)));   
+    }
+    return ans;
+}

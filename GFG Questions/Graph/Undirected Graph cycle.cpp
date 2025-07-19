e59af0cb1095bd6b211c62ce60ecd1b563c5c191
Dhruv-vs <127077342+Dhruv-vs@users.noreply.github.com>
2025-07-19 10:25:26 +0530

    Create Undirected Graph cycle.cpp

---
+++ GFG Questions/Graph/Undirected Graph cycle.cpp
@@ -0,0 +1,33 @@
+class Solution {
+  public:
+  bool dfs(int node,int parent,vector<vector<int>>&adj,vector<bool>&visited){
+      visited[node] = true;
+      for(int child:adj[node]){
+          // if not visited
+          if(!visited[child]){
+              if(dfs(child,node,adj,visited)) return 1;
+          }
+          // if visited
+          else if(child!=parent) return 1;
+      }
+      return 0;
+  }
+    bool isCycle(int V, vector<vector<int>>& edges) {
+        vector<vector<int>>adj(V);
+        // create adj List
+        for(int i=0;i<edges.size();i++){
+            int u = edges[i][0];
+            int v = edges[i][1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        
+        vector<bool>visited(V,false);
+        for(int i=0;i<V;i++){
+            if(!visited[i]){
+                if(dfs(i,-1,adj,visited)) return 1;
+            }
+        }
+        return 0;
+    }
+};
